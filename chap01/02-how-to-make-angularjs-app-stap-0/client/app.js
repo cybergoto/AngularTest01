9d3c6ac3d5d7c095c05fcbd72d1a27c24f9462de
cybergoto <cybergoto@gmail.com>
2016-07-25 11:28:17 +0900

    page29

--- chap01/02-how-to-make-angularjs-app-stap-0/client/app.js
+++ chap01/02-how-to-make-angularjs-app-stap-0/client/app.js
@@ -4,5 +4,21 @@
 (function () {
     'use strict';
 
-    // TODO
+    // angluarJS‚Ímodule’PˆÊ‚ÅApplication‚Æ‚È‚é
+    // 'sns'‚Íhtml‚Ìng-app‚Æ‡‚í‚¹‚Ä‚¨‚­
+    var app = angular.module('sns', []);
+    // module‚ÉV‚µ‚¢Function(TimelineController)‚ğ’Ç‰Á
+    app.controller('TimelineController', function() {
+        this.messages = [];
+        this.message = '';
+
+        this.postMessage = function(){
+            this.messages.push({
+                message: this.message,
+                postDateTime: new Date()
+            });
+            this.message = '';
+        };
+    });
+    //‚±‚±‚Ü‚Å‚ª’Ç‰Á‚³‚ê‚½Function
 })();
