593895fd9aaea891420b20f149d9cdbd92e8e72b
cybergoto <cybergoto@gmail.com>
2016-07-25 13:45:45 +0900

    page35

--- chap01/02-how-to-make-angularjs-app-stap-0/client/app.js
+++ chap01/02-how-to-make-angularjs-app-stap-0/client/app.js
@@ -7,8 +7,35 @@
     // angluarJSはmodule単位でApplicationとなる
     // 'sns'はhtmlのng-appと合わせておく
     var app = angular.module('sns', []);
+
+    //Service利用のサンプル
+    //シンプルなコードでは煩雑になるが、大規模開発では必要となる
+    app.value('messages', []);
+    app.service('TimelineService', ['messages', function(messages){
+        this.postMessage = function (message) {
+            messages.push({
+                message: message,
+                postDateTime: new Date()
+            });
+        };
+    }]);
+    //ControlleへServiceの依存性を注入するサンプル
+    app.controller('TimelineController', ['messages','TimelineService', function(messages, timelineService) {
+        this.messages = messages;
+        this.message = '';
+
+        this.postMessage = function(){
+            timelineService.postMessage(this.message);
+            this.message = '';
+        };
+    }]);
+
     // moduleに新しいFunction(TimelineController)を追加
+    // Classの概念に似ている
+/*
     app.controller('TimelineController', function() {
+        //パラメータはmodelとして扱う
+        //ng-model="TimelineControllerの別名.message"
         this.messages = [];
         this.message = '';
 
@@ -20,5 +47,8 @@
             this.message = '';
         };
     });
+*/
     //ここまでが追加されたFunction
+
+
 })();
