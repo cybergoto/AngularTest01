0eb49ee5940d8caf621c76dea530edeadfa741ae
cybergoto <cybergoto@gmail.com>
2016-07-25 14:44:14 +0900

    lesson1

---
+++ exAng01/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by User on 2016/07/25.
+ */
+(function () {
+    'use strict';
+
+    var app = angular.module('aaa', []);
+
+    app.controller('CalcController', function() {
+        this.param1 = 0;
+        this.param2 = 0;
+        this.result = 0;
+
+        this.calculateParam = function() {
+            this.result = Number(this.param1) + Number(this.param2);
+        };
+    });
+})();
